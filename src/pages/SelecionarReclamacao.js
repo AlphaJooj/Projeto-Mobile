9bf77419a61a0d4f1a34d058d3e5ec5c260d6b96
amitcesar <amitcesar27@gmail.com>
2018-10-22 23:12:00 -0300

    2nd tela implementada

--- src/pages/SelecionarReclamacao.js
+++ src/pages/SelecionarReclamacao.js
@@ -12,7 +12,18 @@ import {
       return(
         <View style={styles.container} >
             <Image style={styles.logo} source={{uri: 'https://static01.nyt.com/images/2016/09/28/us/28xp-pepefrog/28xp-pepefrog-articleLarge.jpg? quality=75&auto=webp&disable=upscale' }} />
-            <Text style={styles.containerLine}> TESTE: </Text>
+            <Text style={styles.containerLine}> Selecione a opção Desejada: </Text>
+            
+            <View style={styles.containerButton}>
+                <TouchableOpacity style={styles.button}> 
+                  <Text style={styles.buttonText}> Postar Reclamação </Text>
+                </TouchableOpacity>
+
+                <TouchableOpacity style={styles.button}> 
+                  <Text style={styles.buttonText}> Ver Reclamações </Text>
+                </TouchableOpacity>
+
+            </View>
          </View>
            
        
@@ -24,23 +35,41 @@ import {
   container: {
     flex: 1,
     backgroundColor: '#F5FCFF',
-
    },
    containerLine:{
-     fontSize: 25,
-    alignSelf: 'center',
-    borderBottomWidth: 1,
-    borderBottomColor: '#bbb',
+    fontSize: 25,
+    alignSelf:'center'
+  
    },
    logo:{
+    flex:1,
     aspectRatio:1,
-    width: 50,
     height:50,
     borderRadius: 45,
     marginTop: 15,
-    padding:5
+    alignSelf:'center'
    
    },
+   button:{
+    backgroundColor: '#6D83F3'  ,
+    borderBottomWidth:1,
+    borderBottomColor: '#bbb',
+    margin:5
+
+  },
+  buttonText:{
+    textAlign: 'center',
+    fontSize: 25,
+    color:"#FFFFFF",
+    fontWeight: 'bold',
+    padding: 10
+    
+  },
+  containerButton: {
+    flex:5,
+    justifyContent: 'center',
+    alignItems: 'center',
+  },
 
  });
     
